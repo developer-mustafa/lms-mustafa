e53c55f0d59e16716bbb12213d2e1d0fe95ba79b
developer-mustafa <mustafarahmanctg@gmail.com>
2024-11-29 15:16:39 +0600

    reverse

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"https://lms-mustafa-z3o8.vercel.app",
+    origin:"http://localhost:5173",
     credentials:true
 }));
  
